2790d168a3e0b7b76504ef5b9d2584abad4ddaca
Enrico Shilisia <173280023+enricoshilisia@users.noreply.github.com>
2026-02-24 12:45:20 +0300

    Fix startup.sh syntax for Oryx compatibility - single line, no comments

--- startup.sh
+++ startup.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash   # ← This shebang is good practice
-
-# Activate Oryx venv (once SCM_DO_BUILD_DURING_DEPLOYMENT=true works)
-. antenv/bin/activate
-
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
-
-uvicorn resilienteco.asgi:application \
-    --host 0.0.0.0 \
-    --port $PORT \
-    --workers 4
+cd /home/site/wwwroot && \
+. antenv/bin/activate && \
+python manage.py migrate --noinput && \
+python manage.py collectstatic --noinput --clear && \
+uvicorn resilienteco.asgi:application --host 0.0.0.0 --port $PORT --workers 4
