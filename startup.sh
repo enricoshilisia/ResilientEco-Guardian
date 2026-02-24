92fce81e425036c6a8646462b9f17df9814280c9
Enrico Shilisia <173280023+enricoshilisia@users.noreply.github.com>
2026-02-24 11:57:11 +0300

    Make startup.sh executable + use bash in startup command

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
-cd /home/site/wwwroot
-python -m venv antenv
-source antenv/bin/activate
-pip install --pre -r requirements.txt
+#!/usr/bin/env bash   # ← This shebang is good practice
+
+# Activate Oryx venv (once SCM_DO_BUILD_DURING_DEPLOYMENT=true works)
+. antenv/bin/activate
+
 python manage.py migrate --noinput
-uvicorn resilienteco.asgi:application --host 0.0.0.0 --port 8000 --workers 4
+python manage.py collectstatic --noinput --clear
+
+uvicorn resilienteco.asgi:application \
+    --host 0.0.0.0 \
+    --port $PORT \
+    --workers 4
