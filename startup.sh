34b3d994a6123f8b63cdace955ecd330c072eb99
Enrico Shilisia <173280023+enricoshilisia@users.noreply.github.com>
2026-02-24 10:23:00 +0300

    Fix startup: create antenv and install dependencies

--- startup.sh
+++ startup.sh
@@ -1,3 +1,9 @@
+@"
+#!/bin/bash
+cd /home/site/wwwroot
+python -m venv antenv
+source antenv/bin/activate
+pip install --pre -r requirements.txt
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
 uvicorn resilienteco.asgi:application --host 0.0.0.0 --port 8000 --workers 4
+"@ | Out-File -FilePath "D:\MaCo\resilienteco-guardian\startup.sh" -Encoding utf8 -NoNewline
